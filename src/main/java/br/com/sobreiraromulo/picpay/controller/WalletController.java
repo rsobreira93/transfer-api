266b8300de7094bb2a100993cb76f431c14b33e8
rsobreira93 <sobreiraromulos@gmail.com>
2024-09-07 07:58:40 -0300

    feat: add wallet controller

---
+++ src/main/java/br/com/sobreiraromulo/picpay/controller/WalletController.java
@@ -0,0 +1,30 @@
+package br.com.sobreiraromulo.picpay.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sobreiraromulo.picpay.controller.dtos.CreateWalletDto;
+import br.com.sobreiraromulo.picpay.entity.Wallet;
+import br.com.sobreiraromulo.picpay.service.WalletService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class WalletController {
+ 
+ @Autowired
+ private WalletService walletService;
+
+
+
+ @PostMapping("/wallets")
+ public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto createWalletDto) {
+     var wallet = walletService.createWallet(createWalletDto);  
+
+     return ResponseEntity.ok(wallet);
+ }
+ 
+}
